4dff3431f6db73fc8bf2106daf230faffd94bfee
sathaporn6104101385 <68616761+sathaporn6104101385@users.noreply.github.com>
2020-10-07 22:36:30 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,163 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <div>
+        <h1 class="text-center" style="color: #cc6699"><b>MY PICTURE</b></h1>
+        <!-- start slide carousel insearch-->
+        <div>
+            <div id="carouselExampleCaptions" class="carousel slide mj" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./assets/img/top.jpg" height="500px" class="d-block w-100" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/img/2.jpg" height="500px" class="d-block w-100" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/img/1.jpg" height="500px" class="d-block w-100" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide</h5>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <br />
+        </div>
+        <!-- end slide carousel-->
+
+        <!-- รูป + ลงคพแนน -->
+        <div class="container border-1 shadow">
+            <h1 class="text-center" style="color: #cc6699"><b>Vote pictures</b></h1>
+            <br />
+            <div class="row">
+                <div class="col-md-7 mb-3">
+                    <img class="bd" src="./assets/img/top.jpg" width="400px" />
+                    <h4>no.1</h4>
+                </div>
+                <br />
+                <div class="col-md">
+                    <counter @cc="sum1" />
+                    <p class="fn">count: {{ total1 }}</p>
+                </div>
+            </div>
+
+            <hr />
+
+            <div class="row">
+                <div class="col-md-7 mb-3">
+                    <img class="bd" src="./assets/img/1.jpg" width="400px" />
+                    <h4>no.2</h4>
+                </div>
+                <div class="col-md">
+                    <counter @cc="sum2" />
+                    <p class="fn">count: {{ total2 }}</p>
+                </div>
+            </div>
+
+            <hr />
+
+            <div class="row">
+                <div class="col-md-7 mb-3">
+                    <img class="bd" src="./assets/img/2.jpg" width="400px" />
+                    <h4>no.3</h4>
+                </div>
+                <div class="col-md">
+                    <counter @cc="sum3" />
+                    <p class="fn">count: {{ total3 }}</p>
+                </div>
+            </div>
+        </div>
+
+        <br />
+        <!-- รูป + ลงคพแนน -->
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import counter from "@/components/counter.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        counter,
+    },
+
+    data: function () {
+        return {
+            total1: 0,
+            total2: 0,
+            total3: 0,
+        };
+    },
+
+    methods: {
+        sum1(counterVavue1) {
+            this.total1 = counterVavue1;
+        },
+        sum2(counterVavue2) {
+            this.total2 = counterVavue2;
+        },
+        sum3(counterVavue3) {
+            this.total3 = counterVavue3;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.bg-img1 {
+    background-color: gray;
+}
+
+.bd {
+    border: 3px solid black;
+}
+
+.bg-comment {
+    background-color: lightcyan;
+}
+
+.fn {
+    font-size: 20px;
+    color: rgb(75, 50, 168);
+    margin-top: 10px;
+}
+
+.mj {
+    margin-bottom: 200px;
+}
+
+h4 {
+    font-size: 20px;
+    color: rgb(75, 50, 168);
+    margin-top: 10px;
 }
 </style>
---
+++ src/components/counter.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div>
+      <button class="btn  " @click="upFunction5">
+        <b class="fs"
+          >👍 <br />
+          <p class="fs2">+5</p></b
+        >
+      </button>
+      <button class="btn  " @click="upFunction10">
+        <b class="fs"
+          >😍 <br />
+          <p class="fs2">+10</p></b
+        >
+      </button>
+      <button class="btn  " @click="upFunction15">
+        <b class="fs"
+          >😆 <br />
+          <p class="fs2">+15</p></b
+        >
+      </button>
+      <button class="btn  " @click="downFunction10">
+        <b class="fs"
+          >😂 <br />
+          <p class="fs2">-10</p></b
+        >
+      </button>
+      <button class="btn  " @click="downFunction20">
+        <b class="fs"
+          >😡 <br />
+          <p class="fs2">-20</p></b
+        >
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    upFunction5: function() {
+      this.counter += 5;
+      this.$emit("cc", this.counter);
+    },
+    upFunction10: function() {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+    upFunction15: function() {
+      this.counter += 15;
+      this.$emit("cc", this.counter);
+    },
+    downFunction10: function() {
+      this.counter -= 10;
+      this.$emit("cc", this.counter);
+    },
+    downFunction20: function() {
+      this.counter -= 20;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+
+<style>
+.fs {
+  font-size: 40px;
+  color: aqua;
+}
+.fs2 {
+  font-size: 20px;
+}
+</style>
